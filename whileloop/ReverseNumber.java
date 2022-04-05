a5c27aa1d47b75c1124a54ac670dfeb26deaad87
unknown <sanjana@example.com>
2022-04-05 22:00:50 +0530

    To reverse the Integer number using while loop

---
+++ whileloop/ReverseNumber.java
@@ -0,0 +1,22 @@
+//Write a program to reverse the integer number ex. Input N = 231 reverse 132
+package com.Bridgelabz.Day02PracticeProblems.whileloop;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter the number:");
+        //Creating Scanner class for taking input from the user
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reverse = 0;
+
+        //it will run the loop until number becomes 0
+        while (number != 0) {
+            //it will get the last digit from number
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+        System.out.println("The reverse of the given number is: " +reverse);
+    }
+}
