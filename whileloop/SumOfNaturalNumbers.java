950aaf063f4fc19d30c76b5ae361df0f4e96f5bc
unknown <sanjana@example.com>
2022-04-05 21:59:13 +0530

    Sum of n Natural number using while loop

---
+++ whileloop/SumOfNaturalNumbers.java
@@ -0,0 +1,24 @@
+//Write a program for the sum of n natural number ex. Input N = 5 output 1+2+3+4+5
+package com.Bridgelabz.Day02PracticeProblems.whileloop;
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        //Creating Scanner class for taking input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number:");
+        int number = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+
+        //while loop will execute the condition until it returns true value
+        while (i <= number) {
+            //adding value of i into the sum variable
+            sum = sum + i;
+            //incrementing value of i by 1
+            i++;
+        }
+        //Displaying sum of n natural numbers
+        System.out.println("Sum of n Natural Numbers is: " +sum);
+    }
+}
