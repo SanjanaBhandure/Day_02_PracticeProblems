62149b11fb34bd2bf208b9f9d424a3e768c6d757
unknown <sanjana@example.com>
2022-04-05 21:57:04 +0530

    To find the Palindrome Number

---
+++ ForLoop/Palindrome.java
@@ -0,0 +1,24 @@
+package com.Bridgelabz.Day02PracticeProblems.whileloop;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int number, temp, sum, remainder;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        number = sc.nextInt();
+        temp = number;
+
+        for (sum = 0; number != 0; number/=10) {
+            remainder = number % 10;
+            sum = (sum * 10)+ remainder;
+        }
+        //palindrome if number and sum are equal
+        if (sum == temp) {
+            System.out.println(temp+ " is a palindrome");
+        } else {
+            System.out.println(temp+" is not a palindrome");
+        }
+    }
+}
